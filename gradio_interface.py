29d351b5f7ad0da9bbf9aba9b382d480d1a70692
gnizama <rhuangt@gmail.com>
2025-03-12 16:22:15 -0300

    Dia de experimentação Problema dos embeddings

---
+++ gradio_interface.py
@@ -0,0 +1,12 @@
+import gradio_interface as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
